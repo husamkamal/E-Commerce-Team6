e48b4b3e4cc14c67ec08e16cfcba40e2518f968a
husamkamal <husamkmal94@gmail.com>
2022-09-29 13:16:41 +0300

    trying to deploy

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const compression = require('compression');
 const cookieParser = require('cookie-parser');
 const { auth } = require('./middlewares/verifyToken');
+const { join } = require('path');
 
 const app = express();
 require('dotenv').config();
@@ -17,8 +18,14 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(auth);
-// app.use(express.static(join(__dirname, '../', 'client', 'build')));
-app.get('/', (req, res) => res.json({ message: 'HELLO SERVER!' }));
+// app.get('/', (req, res) => res.json({ message: 'HELLO SERVER!' }));
 app.use('/api/v1', router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+  app.use('*', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
