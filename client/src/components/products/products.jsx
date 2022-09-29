f4cea89db2079a6ec0907fad6bf2ca058420a767
Mai Mohammed <maymohammedabuabed@gmail.com>
2022-09-29 12:12:30 +0300

    handle products in home page

--- client/src/components/products/products.jsx
+++ client/src/components/products/products.jsx
@@ -2,14 +2,6 @@ import PropTypes from 'prop-types';
 import Card from './card';
 
 function Products({ data }) {
-  // useEffect(() => {
-  //   fetch('/api/v1/name')
-  //     .then((res) => res.json())
-  //     .then((data) => console.log(data))
-  //     .catch((err) => console.log('hello', err));
-  // }, []);
-
-  // const data = [];
   return (
     <ul className="products-list">
       {data.map((el) => (
